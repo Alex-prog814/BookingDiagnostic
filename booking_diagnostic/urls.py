5afc1ac3f0a934cc4b5a06322de0b75e43509a3e
alex <aleks.98.x.28@gmail.com>
2022-02-10 12:49:10 +0600

    Add swagger

--- booking_diagnostic/urls.py
+++ booking_diagnostic/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+from django.conf.urls.static import static
+
+from booking_diagnostic import settings
+
+schema_view = get_swagger_view(title='Booking Diagnostic')
 
 urlpatterns = [
+    path('', schema_view),
     path('admin/', admin.site.urls),
     path('user/', include('applications.user.urls')),
     path('booking/', include('applications.booking.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
